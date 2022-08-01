fbba1183a78cb321f53185bf0e2510e446936aca
BaikSeunghan <tmdgks103@naver.com>
2022-08-01 09:51:20 +0900

    리뷰 작성 및 목록 출력 기능 추가

--- src/main/java/com/example/catchtable/controller/ReservationController.java
+++ src/main/java/com/example/catchtable/controller/ReservationController.java
@@ -1,14 +1,21 @@
 package com.example.catchtable.controller;
 
+import com.example.catchtable.dto.reservation.ReservationResponseDto;
+import com.example.catchtable.security.UserDetailsImpl;
+import com.example.catchtable.service.ReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
 
 //    // 예약 하기
 //    @PostMapping(/api/reservation/store/{storeId})
@@ -18,6 +25,20 @@ public class ReservationController {
 //   //예약 취소
 //    @DeleteMapping(/api/reservation/{id})
 //
+
+    // 사용자 예약 목록 불러오기
+    @GetMapping("/api/users/reservations")
+    public List<ReservationResponseDto> getAllReservations(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return reservationService.getAllReservations(userDetails.getUser().getId());
+    }
+
+    // 사용자 예약 상세 보기
+//    @GetMapping("/api/users/reservations/{storeId}")
+//    public ReservationResponseDto getReservationDtl(@PathVariable Long storeId,
+//                                                    @AuthenticationPrincipal UserDetailsImpl userDetails) {
+//        String userId = userDetails.getUser().getId();
+//        return reservationService.getReservationDtl(storeId, userId);
+//    }
 
 }
 
--- src/main/java/com/example/catchtable/service/ReservationService.java
+++ src/main/java/com/example/catchtable/service/ReservationService.java
@@ -1,11 +1,70 @@
 package com.example.catchtable.service;
 
+import com.example.catchtable.dto.reservation.ReservationResponseDto;
+import com.example.catchtable.dto.review.ReviewResponseDto;
+import com.example.catchtable.model.Reservation;
+import com.example.catchtable.model.Review;
+import com.example.catchtable.model.Store;
+import com.example.catchtable.model.User;
+import com.example.catchtable.repository.ReservationRepository;
+import com.example.catchtable.repository.StoreRepository;
+import com.example.catchtable.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final UserRepository userRepository;
+
+    private final StoreRepository storeRepository;
+
+    /**
+     * 사용자 예약 목록
+     */
+    public List<ReservationResponseDto> getAllReservations(String userId) {
+
+        // 사용자 확인
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new NullPointerException("존재하지 않는 회원입니다."));
+
+        List<ReservationResponseDto> reservationResponseDtos = new ArrayList<>();
+
+        // 사용자 예약 목록 가져오기
+        List<Reservation> myReservations = reservationRepository.findByUserOrderByCreatedAtDesc(user);
+
+        for (Reservation myReservation : myReservations) {
+            ReservationResponseDto reviewResponseDto = new ReservationResponseDto(myReservation);
+            reservationResponseDtos.add(reviewResponseDto);
+        }
+        return reservationResponseDtos;
+    }
+
+    /**
+     *  (사용자) 예약 상세보기
+     */
+//    public ReservationResponseDto getReservationDtl(Long storeId, String userId) {
+//
+//        // 사용자 확인
+//        User user = userRepository.findById(userId).orElseThrow(
+//                () -> new NullPointerException("존재하지 않는 회원입니다."));
+//        // 예약 확인
+////        Reservation reservation = reservationRepository.findByUser(user).orElseThrow(
+////                () -> new NullPointerException("존재하지 않는 회원입니다."));
+//
+//        // 가게 확인
+//        Store store = storeRepository.findById(storeId).orElseThrow(
+//                () -> new NullPointerException("존재하지 않는 가게입니다."));
+//
+//        if(userId == store.ge)
+//
+//        return new ReservationResponseDto(reservation);
+//    }
 }
